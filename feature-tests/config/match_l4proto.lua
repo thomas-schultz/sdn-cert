35030b96a03c2b410be5d2c2a768c4cf9a5ad821
thomas-schultz@tum.de <thomas.schultz@tum.de>
2015-12-18 11:50:15 +0100

    feature-test bug fixes

--- feature-tests/config/match_l4proto.lua
+++ feature-tests/config/match_l4proto.lua
@@ -11,23 +11,24 @@ Feature.state   = "required"
   
 Feature.loadGen = "moongen"
 Feature.files   = "feature_test.lua"
-Feature.lgArgs  = "$file=1 $name $link*"
+Feature.lgArgs  = "$file=1 $name $link=1 $link=2"
+Feature.ofArgs  = "$link=2"
     
 Feature.pkt = Feature.getDefaultPkt()
 
-local new_PROTO = Feature.enum.PROTO.tcp
+Feature.settings = {
+  xIterations = 2,
+  new_PROTO = Feature.enum.PROTO.tcp,
+}
+local conf = Feature.settings
 
-Feature.flowEntries = function(flowData)
-    table.insert(flowData.flows, "ip, nw_proto=" .. Feature.pkt.PROTO .. ", actions=ALL")
-    table.insert(flowData.flows, "ip, nw_proto=" .. new_PROTO .. ", actions=DROP")
+Feature.flowEntries = function(flowData, outPort)
+    table.insert(flowData.flows, string.format("ip, nw_proto=%s, actions=DROP", Feature.pkt.PROTO)
+    table.insert(flowData.flows, string.format("ip, nw_proto=%s, actions=output:%s", conf.new_PROTO, outPort)
   end
-
-Feature.config{
-} 
   
 Feature.modifyPkt = function(pkt, iteration)
     Feature.pkt.PROTO = Feature.new_PROTO
   end
-  
   
 return Feature
